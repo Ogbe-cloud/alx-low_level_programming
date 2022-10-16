7ee935c7ee8f0978ab77a09fb45e09ab24d1060e
Ogbe-cloud <ogbeedel10000@gmail.com>
2022-10-16 08:51:20 -0700

    if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always zero
  */
 int main(void)
 {
@@ -19,7 +19,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
-	else if (a == 5)
+	else if (a == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
@@ -27,6 +27,4 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
-
 	return (0);
-
