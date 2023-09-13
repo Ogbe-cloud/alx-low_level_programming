247220f9a7e01fbd36dade982dc9d4e6a0d3786b
Ogbe-cloud <ogbeedel10000@gmail.com>
2023-09-13 06:39:24 -0700

    error update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main-Entry point of my code
+ * main - Entry point of my code.
  *
- * Return - Always return 0
+ * Return: Always 0
  */
 
 int main(void)
@@ -17,6 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
 
