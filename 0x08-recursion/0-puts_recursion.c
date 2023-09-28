250d81b08c34f2accfe528e24e86fe9b2004941e
Ogbe-cloud <ogbeedel10000@gmail.com>
2023-09-28 10:25:53 -0700

    Recursive commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string and a new line
+ *
+ * @s: a pointer
+ *
+ * RETURN: void
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
