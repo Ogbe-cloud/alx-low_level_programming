c9a56297841c11c251739281c3ad3ffc6f881f92
Ogbe-cloud <ogbeedel10000@gmail.com>
2023-10-12 10:39:55 -0700

    second commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include<stdlib.h>
+#include<stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
