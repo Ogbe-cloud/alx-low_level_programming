1230a47071d4538a7f831f523c3ebf7a60eda028
Ogbe-cloud <104437711+Ogbe-cloud@users.noreply.github.com>
2023-10-12 08:29:34 -0700

    Update 0-main.c

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -15,8 +15,7 @@ int main(void)
     sum = sum_them_all(4, 98, 1024, 402, -1024);
     printf("%d\n", sum);    
     return (0);
-<<<<<<< HEAD
+
 }
-=======
+
 }
->>>>>>> 67de683fe43a67f381779fee0637b8545bd14e27
