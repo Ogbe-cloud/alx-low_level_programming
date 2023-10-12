329c7c37394d8592628ea73f3096909091f2f6df
Ogbe-cloud <ogbeedel10000@gmail.com>
2023-10-12 08:43:45 -0700

    first

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 /**
  * struct typ - Struct operators
- * 
+ *
  * @c : Char
  * @tp : The function associated
 */
