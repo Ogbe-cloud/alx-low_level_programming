2f424352f88fcad12244e00de0eaa2d608a3d2ad
Ogbe-cloud <ogbeedel10000@gmail.com>
2023-10-12 07:51:18 -0700

    first commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all arguements
+ * @n: number of arguements
+ * Return: 0 is n is 0 or return sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i;
+	int result = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(numbers, n);
+
+	for (i = 0; i < n ; i++)
+	{
+		result += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (result);
+
+	}
+
+}
